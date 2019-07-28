f88c19c780b5bcb18af03014fb96b056ea084c4a
rohandash06 <37982545+rohandash06@users.noreply.github.com>
2019-07-28 11:14:15 -0400

    Update bot.js

--- test102/js/bot.js
+++ test102/js/bot.js
@@ -11,7 +11,7 @@ $(function() {
 	// .fbMessenger('message', 'bot', 'When I was 6, my sister was half my age. Now, I\'m 26. How old is she?', { timestamp: now, delay: 800 })
 	// .fbMessenger('message', 'user', '🤔 🤔', { timestamp: now, delay: 4000 })
 	// .fbMessenger('typingIndicator', { delay: 2000 })
-	.fbMessenger('message', 'bot', 'Hello, I\'m <b>Rohan</b> - Graduate Student at UNCC', { timestamp: now, delay: 1500 })
+	.fbMessenger('message', 'bot', 'Hello, I\'m <b>Rohan.</b>I am currently a Graduate Student at the University of North Carolina', { timestamp: now, delay: 1500 })
 	//.fbMessenger('message', 'bot', 'Rolling beans at <b>Walmart Labs</b> 😆', { timestamp: now, delay: 1200 })
 
 	.fbMessenger('showGenericTemplate', [
@@ -23,15 +23,15 @@ $(function() {
 	}
 	], { delay: 1500 })
 
-	.fbMessenger('message', 'user', '🤙', { timestamp: now, delay: 3000 })
-	.fbMessenger('typingIndicator', { delay: 2500 })
-	.fbMessenger('message', 'bot', 'haha', { timestamp: now, delay: 1500 })
+	//.fbMessenger('message', 'user', '🤙', { timestamp: now, delay: 3000 })
+	.fbMessenger('typingIndicator', { delay: 1500 })
+	.fbMessenger('message', 'bot', 'Want to know more about me?', { timestamp: now, delay: 1500 })
 	.fbMessenger('typingIndicator', { delay: 1000 })
-	.fbMessenger('message', 'bot', 'below is some of work and more.. let\'s connect!', { timestamp: now, delay: 1500 })
+	.fbMessenger('message', 'bot', 'Scroll Down!', { timestamp: now, delay: 1500 })
 	.fbMessenger('message', '', '', { timestamp: now, delay: 0 })
 	.fbMessenger('showQuickReplies', [
 		'<a href="#scroll-to-work" id="work-link">Work</a>',
-		'<a href="" target="_blank">Writing</a>',
+		//'<a href="" target="_blank">Writing</a>',
 		'<a href="#scroll-to-about">About</a>',
 		'<a href="resume.pdf" target="_blank">Resume</a>'
 	], { timestamp: now, delay: 1500 })
